4666635432d8994b75b7b7c74c965a74fc52b3d4
Jaehyung Ahn <jh2ekd@gmail.com>
2017-01-12 22:22:47 +0900

    Fixed the blank space between lines bug.

--- app/src/main/java/com/postech/isb/util/IsbSession.java
+++ app/src/main/java/com/postech/isb/util/IsbSession.java
@@ -1062,6 +1062,7 @@ public class IsbSession {
 					
 		// Until meet (end)
 		while(in_loop) {
+			Log.i("newm", "11111111111111");
 			m = p.matcher(msg);
 			if (m.find())
 				current_percent = Integer.valueOf(m.replaceAll("$1")); //Know how many lines.
@@ -1076,14 +1077,20 @@ public class IsbSession {
 				result.append(parsePage(msg));
 			}
 			else if (state == PRESS_J){
+				Log.i("newm", "1111222221111");
 				debugMessage ("More lines. appending..", INFO);
 				String [] list = msg.split("\n");
 				if (list.length == 1)
 					result.append("\n");
 				else if (list[list.length - 1].matches("(?s).*\033\\[\\d+;1H.*"))
 					result.append("\n");
-				else
-					result.append(list[list.length - 2]+"\n");
+				else {
+					String line = list[list.length - 2];
+					if (line.charAt(0) == '\r')
+						line = line.substring(1);
+					Log.i("newm", byteArrayToHex(line.getBytes()));
+					result.append(line + "\n");
+				}
 			}
 			if (!in_loop)
 				break;
